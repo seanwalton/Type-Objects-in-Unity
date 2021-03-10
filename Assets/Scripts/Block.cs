7d500c368b851672b96ee73ebd92898457d0c0e6
Sean Walton <sean@pillbug.zone>
2021-03-10 08:38:03 +0000

    Added all files

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField] private Element myElement;
+    [SerializeField] private int Health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = myElement.Health;
+    }
+
+    void TakeDamage(int damage)
+    {
+        Health -= damage;
+        if (Health <= 0) OnMine();
+    }
+
+    void OnMine()
+    {
+        for (int i = 0; i < myElement.NumberOfItemsToDrop; i++)
+        {
+            Instantiate(myElement.ItemToDrop, transform.position + Random.onUnitSphere, 
+                transform.rotation);
+        }
+        Destroy(gameObject);
+    }
+
+    private void OnMouseDown()
+    {
+        TakeDamage(1);
+    }
+}
+
---
+++ Assets/Scripts/Element.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Element", menuName = "ScriptableObjects/Element", order = 1)]
+public class Element : ScriptableObject
+{
+    public Element Parent;
+    public int Health = 0;
+    public GameObject ItemToDrop = null;
+    public int NumberOfItemsToDrop = 0;
+
+
+    private void OnEnable()
+    {
+        if (Parent == null) return;
+        if (Health == 0) Health = Parent.Health;
+        if (ItemToDrop == null) ItemToDrop = Parent.ItemToDrop;
+        if (NumberOfItemsToDrop == 0) NumberOfItemsToDrop = Parent.NumberOfItemsToDrop;
+    }
+}
+
+
